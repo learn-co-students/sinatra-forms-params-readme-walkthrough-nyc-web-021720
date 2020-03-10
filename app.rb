b6333f8d2e14c4e638d3b4afe2b7da18a88c880b
codenameuriel <u.e.rodriguez@gmail.com>
2020-03-09 22:33:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,5 +7,17 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+  post '/food' do 
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 
 end
+
+# If you create a text field input with <input type="text" name="favorite_food">, whenever the user submits that form, you will be able to access the data entered into the Favorite Foods text box via params[:favorite_food]
+
+# data submitted through a form is passed in the form of a hash
+# params = {
+#   :name => "Sam",
+#   :favorite_food => "Green Eggs and Ham"
+# }
+
